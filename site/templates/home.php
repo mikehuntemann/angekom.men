a0b4b54d3baa9b0dce75b5a395e0fa543992ccf7
mikehuntemann <mail@mikehuntemann.de>
2023-01-03 23:00:56 +0100

    infobox styling details

--- site/templates/home.php
+++ site/templates/home.php
@@ -12,7 +12,7 @@
   <img class="pt-16 pb-8 xl:px-24" src="../assets/png/logos.png">  
 </div>
   <div class="order-2 sm:order-3 w-full pt-12">
-    <div class="mx-auto max-w-4xl text-gray-700 px-6 sm:px-8 px-12 sm:pb-32 pr-0 sm:pr-10 text-lg">
+    <div class="mx-auto max-w-4xl text-gray-700 px-6 sm:px-8 px-12 sm:pb-20 pr-0 sm:pr-10 text-lg">
       <!--- <div class="subpixel-antialiased -ml-2 text-white font-examen-headline-bold text-5xl sm:text-6xl tracking-widest text-bold pb-8">Info</div> --->
         <div class="grid sm:inline-flex">
           <div class="text-lg pr-4 px-20">
@@ -36,16 +36,18 @@
             <iframe src="<?= $video->link() ?>" width="1920" height="1080" scrolling="no" style="border:0px"></iframe>
           </div>
           <div class="pt-12 pb-32">
-            <div class="mx-12 inline-block  my-1 px-4 py-1 bg-gray-300 uppercase rounded-full shadow-md">
+            <div class="ml-12 inline-block  my-1 px-4 py-1 bg-gray-300 uppercase rounded-lg shadow-md">
               <div class="text-ex-dark Roboto text-sm tracking-wide">
                 <?= ucfirst($video->format()) ?>
               </div>
+              
             </div>
-            <div class="text-ex-dark Roboto text-bold text-xl sm:text-3xl text-bold uppercase pb-2 ml-12"><?= $video->title() ?></div>
+            <div class="ml-2 inline-block text-ex-dark Roboto text-sm tracking-wide pr-4 hyphenate pb-2"><?= $video->date()->toDate('Y/m/d') ?></div>
+            <div class="text-ex-dark Roboto text-bold text-xl sm:text-3xl text-bold uppercase py-2 ml-12"><?= $video->title() ?></div>
             <div class="text-ex-dark Roboto text-lg tracking-wide pr-4 hyphenate mx-12"><?= $video->description()->kt() ?></div>
             <div class="text-ex-dark Roboto text-lg tracking-wide pr-4 hyphenate pt-4 "><?= $video->infobox()->kt() ?></div>
             
-            <!--- <div class="text-ex-dark Roboto text-lg tracking-wide pr-4 hyphenate pb-2 ml-12"><?= $video->date()->toDate('Y/m/d') ?></div>
+            <!--- 
             <div class="text-ex-dark Roboto italic bold text-lg sm:text-2xl tracking-wide pr-4 hyphenate py-4"><a class="hover:underline" href="<?= $video->link() ?>" target="_blank">Video ansehen</a></div>
             --->
           </div>
