4f8a44993e76e8910a044b4a0292ecb5e86f1f65
mikehuntemann <mail@mikehuntemann.de>
2023-01-29 17:59:09 +0100

    added version on website

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,29 +1,41 @@
 
 <div class="order-last">
-<div class=" max-w-4xl mx-auto py-4 px-8">
-  <div class="">
-      <div class="flex items-center justify-between sm:justify-start w-1/2 float-left pr-12">
-        <a href="<?= $site->page('home') ?>">
-        <img class="max-h-screen w-full"src="<?= $site->page('info')->image()->url() ?>">
-      </a>
+  <div class=" max-w-4xl mx-auto py-4 px-8">
+    <div class="">
+        <div class="flex items-center justify-between sm:justify-start w-1/2 float-left pr-12">
+          <a href="<?= $site->page('home') ?>">
+          <img class="max-h-screen w-full"src="<?= $site->page('info')->image()->url() ?>">
+        </a>
+        </div>
       </div>
+      <ul class="w-1/2 float-right text-sm">
+        <li class="hover:underline"><a href="http://familienzentrum-lighthouse.de/" target="_blank">Familienzentrum Lighthouse – ein Treffpunkt für alle e.V.</a></li>
+        <li class="hover:underline"><a href="https://www.lpr-hessen.de/medienprojektzentren/kassel/" target="_blank">Medienprojektzentrum Offener Kanal Kassel</a></li>
+        <li class="hover:underline"><a href="https://www.lohfelden.de/de/" target="_blank">Gemeinde Lohfelden</a></li>
+        <li class="hover:underline"><a href="https://www.tgd.de/" target="_blank">Türkische Gemeinde in Deutschland e.V.</a></li>
+        <li class="hover:underline"><a href="https://meinland.info/" target="_blank">MeinLand</a></li> 
+        <li class="hover:underline"><a href="http://www.kulturmachtstark-sh.de/" target="_blank">Kultur macht stark – Bündnisse für Bildung</a></li> 
+        <li class="hover:underline"><a href="https://www.bmbf.de/" target="_blank">Bundesministerium für Bildung und Forschung</a></li> 
+      </ul>
+    <div class="Roboto text-center sm:text-left text-sm sm:text-lg">
+      <ul class="inline-flex pb-4 pt-12 ">
+        <li class="text-ex-dark pr-4">© 2023</li>
+        <!--- <li class="text-ex-dark pr-4 uppercase hover:underline"><a href="https://www.kunsthochschulekassel.de/service/datenschutz.html" target="_blank">Datenschutz</a></li> --->
+        <li class="text-ex-dark pr-4 uppercase hover:underline"><a href="http://familienzentrum-lighthouse.de/impressum.html" target="_blank">Impressum</a></li>
+      </ul>
     </div>
-    <ul class="w-1/2 float-right text-sm">
-      <li class="hover:underline"><a href="http://familienzentrum-lighthouse.de/" target="_blank">Familienzentrum Lighthouse – ein Treffpunkt für alle e.V.</a></li>
-      <li class="hover:underline"><a href="https://www.lpr-hessen.de/medienprojektzentren/kassel/" target="_blank">Medienprojektzentrum Offener Kanal Kassel</a></li>
-      <li class="hover:underline"><a href="https://www.lohfelden.de/de/" target="_blank">Gemeinde Lohfelden</a></li>
-      <li class="hover:underline"><a href="https://www.tgd.de/" target="_blank">Türkische Gemeinde in Deutschland e.V.</a></li>
-      <li class="hover:underline"><a href="https://meinland.info/" target="_blank">MeinLand</a></li> 
-      <li class="hover:underline"><a href="http://www.kulturmachtstark-sh.de/" target="_blank">Kultur macht stark – Bündnisse für Bildung</a></li> 
-      <li class="hover:underline"><a href="https://www.bmbf.de/" target="_blank">Bundesministerium für Bildung und Forschung</a></li> 
-    </ul>
-  <div class="Roboto text-center sm:text-left text-sm sm:text-lg">
-    <ul class="inline-flex pb-4 pt-12 ">
-      <li class="text-ex-dark pr-4">© 2023</li>
-      <!--- <li class="text-ex-dark pr-4 uppercase hover:underline"><a href="https://www.kunsthochschulekassel.de/service/datenschutz.html" target="_blank">Datenschutz</a></li> --->
-      <li class="text-ex-dark pr-4 uppercase hover:underline"><a href="http://familienzentrum-lighthouse.de/impressum.html" target="_blank">Impressum</a></li>
-    </ul>
-  </div>
 
-</div>
+    <div>
+      <a href="https://github.com/mikehuntemann/angekommen-website/releases/">
+        <div class="Roboto text-center text-sm">
+          <div class="text-indigo-400 font-semibold inline">Release v<?= $site->getCurrentVersion() ?> (<?= $site->getTimeSinceRelease() ?>)</div>
+        </div>
+        <?php if($site->getTimeSinceRelease() !== $site->getTimeSinceContentUpdate()): ?>
+        <div class="Roboto text-center text-sm">
+          <div class="text-indigo-400 font-semibold inline">Content Update (<?= $site->getTimeSinceContentUpdate() ?>)</div>
+        </div>
+        <?php endif?>
+      </a>
+    </div>
+  </div>
 </div>
