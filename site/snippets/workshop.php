12b7bffe79549088fed3dca31b65cff1085594e1
mikehuntemann <mail@mikehuntemann.de>
2023-04-14 23:13:29 +0200

    updated workshops and previews

--- site/snippets/workshop.php
+++ site/snippets/workshop.php
@@ -1,12 +1,17 @@
-<?php $workshops = $site->page('workshops')->children(); ?>
+<?php $workshops = $site
+  ->page('workshops')
+  ->children()
+  ->listed(); ?>
 <!--- <?php dump($workshops); ?> -->
 <?php foreach ($workshops as $workshop): ?>
   <!-- <?php dump($workshop); ?> -->
-    
+  
   <div class="mx-auto max-w-4xl text-gray-200 px-6 sm:px-8 pt-8 pb-12 pr-0 sm:pr-10 text-lg ">
     <div class="py-12">
       <div>
-        <a href="<?= $workshop->url() ?>"><?= $workshop->image() ?></a>
+        <a href="<?= $workshop->url() ?>"><?= $workshop
+  ->projectcover()
+  ->toFile() ?></a>
       </div>
       <div class="pt-12 pb-32">
         <div class="ml-12 inline-block  my-1 px-4 py-1 bg-gray-300 uppercase rounded-lg shadow-md">
@@ -15,14 +20,20 @@
           </div>
         </div>
         <div class="ml-2 inline-block text-ex-dark Roboto text-sm tracking-wide pr-4 hyphenate pb-2">
-          <?= $workshop->datestart() ?> - <?= $workshop->dateend() ?>
-        </div>
+          <?= $workshop->dateend()->toDate('dd.MM.YYYY') ?>
+          </div>
         <div class="text-ex-dark Roboto text-bold text-xl sm:text-3xl text-bold uppercase py-2 ml-12">
-          <?= $workshop->theme() ?>
+          <a href="<?= $workshop->url() ?>" >
+          <?= $workshop->theme() ?></a>
         </div>
         <div class="text-ex-dark Roboto text-lg tracking-wide pr-4 hyphenate mx-12">
           <?= $workshop->intro()->kt() ?>
         </div>
+        <div class="text-ex-dark Roboto text-lg tracking-wide pr-4 hyphenate mx-12 pt-4">
+          <a href="<?= $workshop->url() ?>" class="text-blue-700 text-sm tracking-wide pr-4 hyphenate pb-2">
+          Mehr erfahren..</a>
+        </div>
+        <!--
         <div class="text-ex-dark Roboto text-lg tracking-wide pr-4 hyphenate pt-4 "><?= $workshop
           ->infobox()
           ->kt() ?></div>
@@ -38,6 +49,7 @@
           )->title() ?></li>
         <?php endforeach; ?>
         </ul>
+          -->
       </div> <!-- class="pt-12 pb-32" -->
     </div> <!-- class="py-12" -->
   </div> <!-- class="mx-auto max-w-4xl text-gray-200 px-6 sm:px-8 pt-8 pb-12 pr-0 sm:pr-10 text-lg "> -->
--- site/templates/project.php
+++ site/templates/project.php
@@ -1,22 +1,71 @@
 <?php snippet('header'); ?>
 <body>
   <?php snippet('banner'); ?>
+  <!--
+    <div class="max-w-sm pl-16 float-left fixed pt-16">
+    <div class="-ml-4 -mb-4">
+      <div class="pt-10 sm:pt-10 pb-10">
+        <a href="<?= $site->url() ?>"><img class="max-h-screen w-full"src="<?= $site
+  ->page('info')
+  ->image()
+  ->url() ?>">
+      </div>
+    </a>
+    </div>
+  </div> -->
    <div class="mx-auto max-w-4xl text-gray-700 px-6 sm:px-8 px-12 sm:pb-20 pr-0 sm:pr-10 text-lg">
   <?php $content = $page->content(); ?>
-  <?= $content->title() ?>
-  <?= $content->theme() ?>
-  <?= $content->datestart() ?>
-  <?= $content->dateend() ?>
+  <div class="py-12">
+   <img src="<?= $content
+     ->projectcover()
+     ->toFile()
+     ->url() ?>" >
+  </div>
+  <div class="pt-12 pb-32">
+    <div class="inline-block  my-1 px-4 py-1 bg-gray-300 uppercase rounded-lg shadow-md">
+      <div class="text-ex-dark Roboto text-sm tracking-wide">
+        <?= ucfirst($content->title()) ?>
+      </div>
+    </div>
+    <div class="ml-2 inline-block text-ex-dark Roboto text-sm tracking-wide pr-4 hyphenate pb-2">
+      <?= $content->dateend()->toDate('dd.MM.YYYY') ?>
+      </div>
+    <div class="text-ex-dark Roboto text-bold text-xl sm:text-3xl uppercase py-2 pb-6">
+      <a href="<?= $content->url() ?>" >
+      <?= $content->theme() ?></a>
+    </div>
+    <?php $projects = $page->videoslots()->toStructure(); ?>
+    <?php foreach ($projects as $project): ?>
+      <div class="Roboto text-bold text-2xl"><?= $project->title() ?></div>
+      <div><?= $project->description()->kt() ?></div>
+      <div><?= $project->format() ?></div>
+      <!-- if format = video: use link -->
+      <?php $photos = $project->photo()->toFiles(); ?>
+      <?php foreach ($photos as $photo): ?>
+        <img class="max-h-80 mx-auto py-4" src="<?= $photo->url() ?>">
+      <?php endforeach; ?>
+      <?php if ($project->link()->isNotEmpty()): ?>
+      <div class="aspect-w-16 aspect-h-9">
+        <iframe src="<?= $project->link() ?>" width="1920" height="1080" scrolling="no" style="border:0px"></iframe>
+      </div>
+      <?php endif; ?>
+    <?php endforeach; ?>
+
+
+  <div class="max-w-4xl pt-20">
   <img src="<?= $content
     ->groupphoto()
     ->toFile()
     ->url() ?>">
-        <ul>
+        <ul class=""> <!-- comma-list -->
         <?php foreach ($content->participants()->split() as $participant): ?>
-          <li class="text-ex-dark Roboto text-lg tracking-wide pr-4 hyphenate mx-12 ">
+          <li class=" inline mx-6 text-ex-dark Roboto text-lg tracking-wide hyphenate">
             <?= page($participant)->title() ?></li>
         <?php endforeach; ?>
         </ul>
-          </div>
+        </div>
+    </div>
+    </div>
+  </div>
 </body>
 <?php snippet('footer'); ?>
